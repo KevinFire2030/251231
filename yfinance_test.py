61cc830a5525881c36bcd588cfd4e11358b6a088
KevinFire2030 <109524169+KevinFire2030@users.noreply.github.com>
2023-10-04 22:19:07 +0900

    SPY vs SPY 터틀

--- yfinance_test.py
+++ yfinance_test.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import yfinance as yf
 import pandas_datareader.data as web
 
@@ -108,11 +109,38 @@ df_3mo = yfObj.history(start="2023-01-20", end="2023-09-25", interval='3mo')
 
 """
 
+"""
 tickers = ["NQ=F",""]
 
 
 yfObj = yf.Tickers(tickers)
 
 df_1d = yfObj.history(start="2023-09-20", end="2023-09-25", interval='1d')
+"""
+
+"""
+tickers = ["SPY", "QQQ"]
+
+yfObj = yf.Tickers(tickers)
+#df = yfObj.history(start=self.start, end=self.end, interval='1h', back_adjust=True, auto_adjust=True, prepost=True)
+df = yfObj.history(start="2023-09-01", end="2023-09-25")
+df.drop(['Capital Gains', 'Open', 'Dividends', 'Stock Splits', 'Volume'], inplace=True, axis=1)
+
+df.ffill(inplace=True)
+df = df.swaplevel(axis=1)
+
+df.drop(['QQQ'], inplace=True, axis=1)
+
+df.to_excel('data_2310040045.xlsx')
+
+"""
+
+#df = yf.Ticker('SPY').history(start="2023-09-20", end="2023-09-25")
+
+#df = yf.download('SPY', start="2023-09-20", end="2023-09-25")
+
+#df.to_excel('data_2310040134.xlsx')
+
+df = pd.read_excel('data_2310040134_2.xlsx')
 
 print("나는 25년 12월 31일 은퇴했다")
